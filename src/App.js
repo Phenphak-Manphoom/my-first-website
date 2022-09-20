909b14fd66f12a36b80ea52c727042b04f86a824
phenphak <phenphak.forlearn@gmail.com>
2022-09-20 13:47:42 +0700

    add my first website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Stack } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import DisplayJohn from "./components/DisplayJohn";
+import {JohnProfile}  from "./components/DisplayJohn";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <Stack spacing={4} direction="row" align="center">
+          <Button colorScheme="gray" size="xs">
+            Button
+          </Button>
+          <Button colorScheme="red" size="sm">
+            Button
+          </Button>
+          <Button colorScheme="orange" size="md">
+            Button
+          </Button>
+        </Stack>
+
+        <Stack direction="row" spacing={6}>
+          <Spinner color="red.500" size="md" />
+          <Spinner color="blue.500" size="lg" />
+          <Spinner color="gray.500" size="xl" />
+        </Stack>
+
+        <Stack spacing={3}>
+          <Input variant="outline" placeholder="Outline" />
+          <Input variant="filled" placeholder="Filled" />
+          <Input variant="flushed" placeholder="Flushed" />
+        </Stack>
+      </ChakraProvider>
+      <div className="display-john">
+      <DisplayJohn />
+      <JohnProfile />
+      </div>
     </div>
   );
 }
---
+++ src/components/DisplayJohn.js
@@ -0,0 +1,15 @@
+function DisplayJohn() {
+  return <h1>Hello, I'm John</h1>;
+}
+export function JohnProfile() {
+  return (
+    <div className="list-items">
+      <ul>
+        <li>Full Stack Developer</li>
+        <li>React.JS Developer</li>
+        <li>Love Mookrata</li>
+      </ul>
+    </div>
+  );
+}
+export default DisplayJohn;
